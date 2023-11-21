1b70552fe915d1a0de7eea992d446ae9f09ae0c5
erin-hu24 <erinjhu@gmail.com>
2023-11-21 13:06:22 -0500

    Created Buttons

--- graphics.java
+++ graphics.java
@@ -3,20 +3,48 @@ import javax.swing.*;
 import java.awt.event.*;
 import javax.swing.event.*;
 
-public class graphics implements ActionListener{
-    //Properties
+public class graphics implements ActionListener {
+    // Properties
     JFrame theFrame = new JFrame("SOH CAH TOA Simulator");
     panelgraphics thePanel = new panelgraphics();
-    Timer newFrame = new Timer(1000/48, this);
+    Timer newFrame = new Timer(1000 / 48, this);
+    // Buttons
+    JButton sinButton = new JButton("Sin");
+    JButton cosButton = new JButton("Cos");
+    JButton tanButton = new JButton("Tan");
+    // Text Fields
+    JTextField angleText = new JTextField();
+    JTextField sideAText = new JTextField();
+    JTextField sideBText = new JTextField();
+    // Labels
+    JLabel sideALabel = new JLabel("Side A");
+    JLabel sideBLabel = new JLabel("Side B");
+    JLabel angleALabel = new JLabel("Angle A");
 
-    //Methods
-    public void actionPerformed(ActionEvent evt){
+    // Methods
+    public void actionPerformed(ActionEvent evt) {
 
     }
 
-    //Constructor
-    public graphics(){
+    // Constructor
+    public graphics() {
         thePanel.setPreferredSize(new Dimension(1280, 720));
+        thePanel.setLayout(null);
+        // Buttons
+        sinButton.setSize(265, 40);
+        sinButton.setLocation(20, 88);
+        thePanel.add(sinButton);
+        cosButton.setSize(265, 40);
+        cosButton.setLocation(20, 214);
+        thePanel.add(cosButton);
+        tanButton.setSize(265, 40);
+        tanButton.setLocation(20, 340);
+        thePanel.add(tanButton);
+        // note: set up ActionListener for buttons
+        // Text Fields
+
+        // Labels
+
         theFrame.setContentPane(thePanel);
         theFrame.pack();
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
