886af99ce61fde384b9fb2227f3e592fc5064de0
erin-hu24 <erinjhu@gmail.com>
2023-11-24 14:27:07 -0500

    Added panel for testing module

---
+++ testingpanel.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+public class testingpanel extends JPanel {
+
+    // Properties
+
+    // Methods
+    public void paintComponent(Graphics g){
+        
+    }
+
+    // Constructor
+    public testingpanel(){
+
+    }
+    
+}
