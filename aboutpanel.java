ff259efdd505ca9bffbd3d4a0bbe862787af7c26
erin-hu24 <erinjhu@gmail.com>
2023-11-24 14:00:10 -0500

    Added about and help page

--- aboutpanel.java
+++ aboutpanel.java
@@ -1,18 +1,26 @@
 import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class aboutpanel extends JPanel{
     //Properties
+    BufferedImage imgAbout = null;
 
     //Methods
     public void paintComponent(Graphics g){
-        g.drawString("About",20,20);
-        g.drawString("SOH CAH TOA Simulator was programmed by Nicholas Ching and Erin Hu", 20, 50);
+       g.drawImage(imgAbout, 0, 0, null);
 
     }
     
     //Constructor
     public aboutpanel(){
-
+        try{
+            imgAbout = ImageIO.read(new File("about.png"));
+        }
+        catch(IOException e){
+            System.out.println("Unable to load image");
+        }
     }
 }
--- helppanel.java
+++ helppanel.java
@@ -1,16 +1,25 @@
 import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
 import javax.swing.*;
+
 
 public class helppanel extends JPanel{
     //Properties
-
+    BufferedImage imgHelp = null;
     //Methods
     public void paintComponent(Graphics g){
-        
+        g.drawImage(imgHelp, -5, 0, null);
     }
     
     //Constructor
     public helppanel(){
-
+        try{
+            imgHelp = ImageIO.read(new File("help.png"));
+        }
+        catch(IOException e){
+            System.out.println("Unable to load image");
+        }
     }
 }
